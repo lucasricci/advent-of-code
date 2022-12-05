ce7de379c3d9eadf6a056e6cca64544df9a402e1
Lucas Ricci <llricci@protonmail.com>
2022-12-05 14:28:19 -0300

    Initial commit

--- 2022/folders.py
+++ 2022/folders.py
@@ -7,6 +7,5 @@ for c in range(1, 32):
     folders.append(f'Day-{c}')
 
 for folder in folders:
-    os.mkdir(os.path.join(root_path,folder))
+    os.makedirs(os.path.join(root_path,folder))
     os.system(f'touch /home/lucas/advent-of-code/2022/{folder}/program.py')
-
